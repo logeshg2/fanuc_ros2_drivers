a5fc5eb2ecdb2096b7129b01bbd1e871745b1c10
Logesh <logeshg248@gmail.com>
2026-02-23 20:38:22 +0530

    changes - added adjoint wrench transform

--- src/fanuc_force_controller/fanuc_force_controller/admittance_control.py
+++ src/fanuc_force_controller/fanuc_force_controller/admittance_control.py
@@ -56,18 +56,11 @@ class AdmittanceControl(Node):
         ])
         self.maIdx = 0
 
-        # force control parameter
+        # force control parameter (compute initial force offset)
         Fz, Mx, My = self.bot.read_force_sensor_values()     # [Fz, Mx, My]
         self.FT_ideal = np.array([0.0, 0.0, Fz, Mx, My, 0.0])
-        self.H = np.array([
-            [0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
-            [0.0, 0.1, 0.0, 0.0, 0.0, 0.0],
-            [0.0, 0.0, 0.1, 0.0, 0.0, 0.0],
-            [0.0, 0.0, 0.0, 0.1, 0.0, 0.0],
-            [0.0, 0.0, 0.0, 0.0, 0.1, 0.0],
-            [0.0, 0.0, 0.0, 0.0, 0.0, 0.1]
-        ])
 
+        # mass coefficient matrix
         self.mass = np.array([
             [8, 0.0, 0.0, 0.0, 0.0, 0.0],
             [0.0, 8, 0.0, 0.0, 0.0, 0.0],
@@ -76,6 +69,7 @@ class AdmittanceControl(Node):
             [0.0, 0.0, 0.0, 0.0, 3, 0.0],
             [0.0, 0.0, 0.0, 0.0, 0.0, 0.5]
         ])
+        # damping coefficient matrix
         self.damp = np.array([
             [60, 0.0, 0.0, 0.0, 0.0, 0.0],
             [0.0, 60, 0.0, 0.0, 0.0, 0.0],
@@ -85,6 +79,19 @@ class AdmittanceControl(Node):
             [0.0, 0.0, 0.0, 0.0, 0.0, 15]
         ])
         self.arm_des_twist = np.zeros((6,1))
+
+        # transform from FT sensor to EE
+        self.fTe = np.eye(4)
+        # TODO: need to identify translation
+        self.fTe[0:3, 0:3] = Rotation.from_euler("xyz", [0, 0, 90], degrees=True).as_matrix()
+
+        # adjoint transformation matrix (to transform force in FT sensor frame to EE frame)
+        # NOTE: this adjoint wrench transform is different from twist adjoint transform matrix
+        self.AD_fTe  = np.zeros((6,6))
+        self.AD_fTe[0:3, 0:3] = self.fTe[0:3, 0:3]
+        self.AD_fTe[3:6, 3:6] = self.fTe[0:3, 0:3]
+        # TODO: need to add translation part to AD_fTe
+
 
         # ros2 comm variables
         self.inc_srv_trig = self.create_service(SetBool, '/trigger_servoing', self.trigger_servoing_cb)
@@ -146,28 +153,27 @@ class AdmittanceControl(Node):
             # print(Fz, Mx, My)
             
             curFT_val = np.array([0.0, 0.0, Fz, Mx, My, 0.0])
-            measuredFT = curFT_val - self.FT_ideal
-            measuredFT = np.array(measuredFT).reshape((6,1))
+            wrench_ext_ft = curFT_val - self.FT_ideal                 # subtract offset force from FT measurement
+            wrench_ext_ft = np.array(wrench_ext_ft).reshape((6,1))    # in FT sensor frame
+
+            # wrench from FT sensor frame to ee frame 
+            wrench_ext_ee = self.AD_fTe.T @ wrench_ext_ft          # 6x6 @ 6x1
 
             # compute robot jacobian
             jac = pinocchio.computeFrameJacobian(self.robotModel, self.robotData, np.array(rad_arr), self.eeFrameId)
 
-            """
-            # cartesian / ee velocity computation (based on force applied)
-            cartAcc = (jac @ np.linalg.pinv(self.H) @ np.array(jac).T) @ measuredFT
-            cartVel = cartAcc * 0.001
-            """
-
-            arm_des_acc = np.linalg.pinv(self.mass) @ ((-self.damp @ self.arm_des_twist) + measuredFT)
+            arm_des_acc = np.linalg.pinv(self.mass) @ ((-self.damp @ self.arm_des_twist) + wrench_ext_ee)
             self.arm_des_twist = arm_des_acc * 0.1
-            cartVel = self.arm_des_twist.flatten()
-            cartVel[3], cartVel[4] = cartVel[4], cartVel[3]
-            cartVel[3] *= -1
-            cartVel[4] *= -1
+            # TODO: change the integration (dt) to proper measured dt
+            eeVel = self.arm_des_twist.flatten()
+            # cartVel[3], cartVel[4] = cartVel[4], cartVel[3]
+            # cartVel[3] *= -1
+            # cartVel[4] *= -1
 
-            print(np.round(cartVel, 2))
+            print(np.round(eeVel, 2))
             
-            joint_vels = (np.linalg.pinv(jac) @ np.array([cartVel]).reshape((6,1)))
+            # compute joint velocity from 
+            joint_vels = (np.linalg.pinv(jac) @ np.array([eeVel]).reshape((6,1)))
             joint_vels = joint_vels.flatten()
 
             # compute target joint angle from target velocity
